162e362088b9d752d8dbac57c26870afb1ab60c3
thongdoan <thongdoan742@gmail.com>
2024-11-01 16:53:52 +0700

    4-53-11/1

--- FormLogin.cs
+++ FormLogin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -36,5 +37,49 @@ namespace howto_weather_forecast2
         {
             this.Close();
         }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=THONGDOAN\SQLEXPRESS;Initial Catalog=Users;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Lấy tên người dùng và mật khẩu từ các ô nhập liệu
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            try
+            {
+                String querry = "SELECT * FORM login WHERE username = '" + textBox1 + "' AND password = '" + textBox2 + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                if ( dtable.Rows.Count > 0 )
+                {
+                    username = textBox1.Text;
+                    password = textBox2.Text;
+
+                    Form form = new Form();
+                    form.Show();
+                    this.Hide();
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid login details", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
+            }
+            catch{
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
+            MessageBox.Show(password, username, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
     }
 }
