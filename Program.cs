96fc4a2109aee4e2930c6d6c45b9cc39225dcf16
Phuc2004-w <phucnht04@gmail.com>
2024-11-19 17:29:25 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
-using howto_weather_forecast2;
 
-namespace dubaothoitiet
+namespace ungdungthoitiet
 {
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Điểm bắt đầu của ứng dụng.
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new txt()); // Tạo và hiển thị form chính
         }
     }
 }
